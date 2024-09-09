43827e4ad409e3d826e4a0873f0fff12dfe10ec6
Carlos Fuch <httpcarlosx@gmail.com>
2024-09-09 10:05:06 -0300

    feat: recuperação de status de operações de remediação

--- main.py
+++ main.py
@@ -14,21 +14,31 @@ client_id, api_key, api_url = load_configuration()
 sdk_info: HarmonyEndpointSDKInfo = HarmonyEndpoint.info()
 print(sdk_info)
 
-# Connect to management using CloudInfra API credentials
+# Conexão ao portal de gerenciamento usando as credenciais para a API CloudInfra
 he.connect(infinity_portal_auth=InfinityPortalAuth(
-        client_id=client_id, # The "Client ID"
-        access_key=api_key, # The "Secret Key"
-        gateway=api_url # The "Authentication URL"
+        client_id=client_id, # O "Client ID"
+        access_key=api_key, # A "Secret Key"
+        gateway=api_url # A "Authentication URL"
         )) 
 
-# Query the API operation
-rules_metadata_res = he.policy_general_api.get_all_rules_metadata(header_params={ "x-mgmt-run-as-job": 'off'})
-print(rules_metadata_res.payload)  # Your rulebase metadata
+# Requisição para receber metadados das policies
+print("Requisitando metadados das policies")
+#rules_metadata_res = he.policy_general_api.get_all_rules_metadata(header_params={ "x-mgmt-run-as-job": 'off'})
+#print(rules_metadata_res.payload)  # Metadados das policies
+print("\n-----------------------------------------\n")
 
-# Also you can query this operation using job, no extra logic required, in the background, it will trigger a job and will pull the status till it finish and return the final results. 
+# A mesma operação, mas usando um job no background -- o resultado final será enviado quando o job encerrar
 rules_metadata_res = he.policy_general_api.get_all_rules_metadata(header_params={ "x-mgmt-run-as-job": 'on'})
 print(rules_metadata_res.is_job)  # True
-print(rules_metadata_res.payload)  # Your rulebase metadata
+print(rules_metadata_res.payload)  # Metadados das policies
+print("\n-----------------------------------------\n")
 
-# Once finish, disconnect to stop all background session management. 
+# Requisição de status de operações de remediação
+# Precisa ser job:on
+print("Requisitando status de todas as operações de remediação")
+remediation_statuses = he.remediation_response_general_api.get_all_remediation_operation_statuses(header_params={ "x-mgmt-run-as-job": 'on'})
+print(remediation_statuses.payload)
+print("\n-----------------------------------------\n")
+
+# Encerramento da sessão
 he.disconnect()
